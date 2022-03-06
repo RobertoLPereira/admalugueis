2a2a5d82c2c5f51708ee4e5c2c398fea26d16b6f
RobertoLPereira <roberto.lpereira65@gmail.com>
2022-03-06 18:40:20 -0300

    Ajustes
    Ajustes

--- routes/portal_conteudo_rt.js
+++ routes/portal_conteudo_rt.js
@@ -2,6 +2,6 @@ module.exports = app => {
     const Pessoas = app.db.models.pessoas;
     app.route('/')
     .get((req, res) => {
-      res.render('../views/pages/Portal_Conteudo.ejs');
+      res.render('/pages/Portal_Conteudo.ejs');
     });
   };
